787b1b79c052b9feb53d71eb201bd89c395a46ff
neojarvis <techadmin@iamneo.ai>
2025-06-25 19:14:22 +0000

    Auto-save-commit-26/06/2025--00:44:18

---
+++ airflow.sh
@@ -0,0 +1,22 @@
+pip install "apache-airflow==2.8.1" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.1/constraints-3.11.txt"
+
+# Set environment variables (use your own values as needed)
+export AIRFLOW_HOME=~/airflow
+
+# Initialize DB
+airflow db init
+
+# Create admin user
+airflow users create \
+  --username admin \
+  --firstname Azhagu \
+  --lastname Venkadesh \
+  --role Admin \
+  --email admin@example.com \
+  --password admin
+
+# Start web server (default: http://localhost:8080)
+airflow webserver --port 8080 &
+
+# Start scheduler (in background)
+airflow scheduler &
